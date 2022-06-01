219c57494a904daff14ad801bac9e19daa80be3f
Jonathan Danse <j.danse@prestaedit.com>
2022-06-01 09:41:14 +0200

    Fix used font

--- global-components/BlogTags.vue
+++ global-components/BlogTags.vue
@@ -13,7 +13,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-@import url('https://fonts.googleapis.com/css?family=Merriweather:400,400i,700&display=swap')
+@import url('https://fonts.googleapis.com/css?family=Proxima+Nova:400,400i,700&display=swap')
 .blog-tags
   min-height:500px
 .blog-tag
